926e63a11715bbe2dab44dfb18f10c1440b69c87
Warley Afonso <warleysilva365@gmail.com>
2022-04-16 06:21:09 -0300

    Adicionando Menu-Hamburguer e a responsividade do site

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuSection = document.querySelector('.menu-section');
+const menuBurguer = menuSection.querySelector('.menu-burguer');
+
+menuBurguer.addEventListener('click', () => {
+  menuSection.classList.toggle('active')
+})
